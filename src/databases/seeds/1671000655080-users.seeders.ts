05192a2c65a3271d1be48f43a7f31a2aa2d24d45
padi-dev-ducla <ducla@paditech.com>
2024-11-01 23:40:21 +0700

    config seed data

---
+++ src/databases/seeds/1671000655080-users.seeders.ts
@@ -0,0 +1,84 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+import { RoleEnum } from '@enum/role.enum';
+import { UsersEntity } from '@entities';
+
+export default class CreateUsers implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    const current = new Date();
+    const firstThisWeek = current.getDate() - current.getDay() + 1;
+    const users = [
+      {
+        id: 1,
+        email: 'admin@gmail.com',
+        password:
+          '$2b$10$8NoeFbeBargsDsClhpfkDexfk0RtV6kDSJa/yTOwJ3Wbo3n6e3k/.', //123456
+        role: RoleEnum.ADMIN,
+        created_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+        updated_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+      },
+      {
+        id: 2,
+        email: 'hr@gmail.com',
+        password:
+          '$2b$10$8NoeFbeBargsDsClhpfkDexfk0RtV6kDSJa/yTOwJ3Wbo3n6e3k/.', //123456
+        role: RoleEnum.HR,
+        created_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+        updated_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+      },
+      {
+        id: 3,
+        email: 'hr2@gmail.com',
+        password:
+          '$2b$10$8NoeFbeBargsDsClhpfkDexfk0RtV6kDSJa/yTOwJ3Wbo3n6e3k/.', //123456
+        role: RoleEnum.HR,
+        created_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+        updated_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+      },
+      {
+        id: 4,
+        email: 'user@gmail.com',
+        password:
+          '$2b$10$8NoeFbeBargsDsClhpfkDexfk0RtV6kDSJa/yTOwJ3Wbo3n6e3k/.', //123456
+        role: RoleEnum.CANDIDATE,
+        created_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+        updated_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+      },
+      {
+        id: 5,
+        email: 'user2@gmail.com',
+        password:
+          '$2b$10$8NoeFbeBargsDsClhpfkDexfk0RtV6kDSJa/yTOwJ3Wbo3n6e3k/.', //123456
+        role: RoleEnum.CANDIDATE,
+        created_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+        updated_at: new Date(
+          new Date().setDate(firstThisWeek - 7),
+        ).toISOString(),
+      },
+    ];
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(UsersEntity)
+      .values(users)
+      .execute();
+  }
+}
---
+++ src/entities/users.entity.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
+import { BaseEntity } from '@entities/base.entity';
+import { RoleEnum } from '@common/enum/role.enum';
+
+@Entity()
+export class Users extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @Index({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: RoleEnum;
+}
