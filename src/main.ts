094d570fcdd3385ccb5e85c32077fce4c30de39d
padi-dev-ducla <ducla@paditech.com>
2024-11-02 10:44:57 +0700

    update package

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { env } from '@env';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  // PARSE REQUESTS
+  app.use(urlencoded({ extended: true }));
+  app.use(
+    json({
+      limit: '10mb',
+    }),
+  );
+
+  const port = env.app.port || 3000;
+  await app.listen(port);
+  console.log(`app running at http://localhost:${port}/`);
 }
 bootstrap();
