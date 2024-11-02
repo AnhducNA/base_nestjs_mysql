79bf2e9b8cb0c4cdc3f54a65ed7209180c6d1e68
padi-dev-ducla <ducla@paditech.com>
2024-11-02 16:00:47 +0700

    config cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { env } from '@env';
 import { json, urlencoded } from 'express';
+import helmet from 'helmet';
+
+const allowedOrigins = [
+  // WEBSITES
+  'http://localhost',
+];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,11 +15,22 @@ async function bootstrap() {
 
   // PARSE REQUESTS
   app.use(urlencoded({ extended: true }));
-  app.use(
-    json({
-      limit: '10mb',
-    }),
-  );
+  app.use(json({ limit: '10mb' }));
+
+  //security
+  app.use(helmet());
+  // Enable Cross-origin resource sharing for a list of domains
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.some((o) => origin.startsWith(o))) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origin not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const port = env.app.port || 3000;
   await app.listen(port);
